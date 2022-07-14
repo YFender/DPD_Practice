e9d69bde9004fc16c8bf7675865fb6b75bc8ac47
yurik <yurik2159@inbox.ru>
2022-07-14 15:06:06 +0300

    f

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
-import flask
-import kafka
+import logging
+from sqlite3 import connect
 
-app = flask.Flask(__name__)
-producer = kafka.KafkaProducer(bootstrap_servers='localhost:9092')
+from flask import Flask, request
 
+app = Flask(__name__)
+conn = connect("database.sqlite", check_same_thread=False)
+logger = logging.Logger("example")
 
-@app.post("/foobar")
-@app.post("/foobar/")
-def foobar():
-    if flask.request.is_json:
-        print(flask.request.json)
-        future = producer.send("foobar_topic", f"{flask.request.json}".encode())
-        if future.failed():
-            for i in range(100):
-                future = producer.send("foobar_topic", flask.request.json)
-                if not future.failed():
-                    break
-                else:
-                    if i == 99:
-                        return "500"
-            return "200"
+@app.post("/write_call")
+def write_call():
+    """
+    Данная функция демонстрирует идею записи звонков в колл-лист, параметры их кол-во может отличаться от настоящих.
+    В качестве примера используется таблица Call_list cо столбцами:
+    Call_ID - ID номер звонка;
+    Phone_number - телефонный номер клиента;
+    Call_date - дата звонка;
+    Call_time - время звонка;
+    Is_called_back - столбец с булевым значением, отображающий статус перезвона, где 0 означает, что сотрудник еще не перезвонил клиенту, а 1 - перезвонил;
+    """
+    if request.is_json:
+        data = request.json # предполагается, что json имеет ключи: {"phone_number", "call_date", "call_time"}
+        cursor = conn.cursor()
+        cursor.execute(f'SELECT * FROM Call_list WHERE Phone_number = "{data["phone_number"]}" ')
+        result = cursor.fetchall()
+
+        if result:
+            for i in reversed(result):
+                if i[4] == 0 or i[4] == "0": # проверка Is_callen_back в каждой записи.
+                    return 400, "THIS NUMBER HAS BEEN ALREADY RECORDED"
+            cursor.execute(f'INSERT INTO Call_list VALUES(Null, "{data["phone_number"]}", "{data["call_date"]}", "{data["call_time"]}", 0)')
+            conn.commit()
+            return 200, "DONE"
         else:
-            return "200"
+            cursor.execute(f'INSERT INTO Call_list VALUES(Null, "{data["phone_number"]}", "{data["call_date"]}", "{data["call_time"]}", 0)')
+            conn.commit()
+            return 200, "DONE"
+    else:
+        return 400, "NOT JSON"
+
 
 if __name__ == "__main__":
     app.run(port=5050)
--- test2.py
+++
@@ -1,8 +0,0 @@
-from kafka import KafkaProducer
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
-for _ in range(1):
-    print('some_message_bytes'.encode())
-    future = producer.send('TutorialTopic', 'some_message_bytes'.encode())
-    print(future.get())
-    future = producer.send('foobar', 'some_message_bytes'.encode())
-    print(future.get())
